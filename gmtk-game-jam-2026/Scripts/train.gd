9461ac524ef86771d5468f886c555f5770ee03fd
Víctor Martínez Moreno <vicmar20@ucm.es>
2026-07-23 12:42:51 +0200

    New train movement

--- gmtk-game-jam-2026/Scripts/train.gd
+++ gmtk-game-jam-2026/Scripts/train.gd
@@ -13,6 +13,9 @@ var currDir: Vector2i
 # Next tile´s index
 var nextTile: Vector2i
 
+#Distance to the currTile that makes train changes its direction inmediatly
+var fixDistance = 5
+
 func _ready() -> void:
 	#Init current Tile and the next one
 	#Also init current direction
@@ -28,31 +31,40 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var input_vector = Input.get_vector("Left", "Right", "Up", "Down")
 	
+	
+	var distanceNext = global_position.distance_to(tilemap.map_to_local(nextTile))
+	var distanceCurr = global_position.distance_to(tilemap.map_to_local(currTile))
+	
 	#Train´s direction changes
 	if input_vector != Vector2.ZERO:
-		#The train can´t go backwards
-		if input_vector.x == 0 and input_vector.y != -currDir.y:
-			saveDir = Vector2i(0, input_vector.y)
-		elif input_vector.y == 0 and input_vector.x != -currDir.x:
-			saveDir = Vector2i(input_vector.x, 0)
-	
+		#The train can´t go backwards or forward
+		#If the train is only a few pixels away, changes inmediatly the train´s dir and relocates it.
+		#Otherwise, saves the new direction.
+		if input_vector.x == 0 and abs(input_vector.y) != abs(currDir.y):
+			if distanceCurr < fixDistance : changeDir(Vector2i(0, input_vector.y))
+			else: saveDir = Vector2i(0, input_vector.y)
+		elif input_vector.y == 0 and abs(input_vector.x) != abs(currDir.x):
+			if distanceCurr < fixDistance : changeDir(Vector2i(input_vector.x, 0))
+			else: saveDir = Vector2i(input_vector.x, 0)
 	#When you arrive the next Tile
 	#Updates current and next Tile indexes
 	#Repositions the train on the new tile (just in case)
-	var distance = global_position.distance_to(tilemap.map_to_local(nextTile))
-	if distance < 2:
-		global_position = tilemap.map_to_local(nextTile)
-		#If the player has been changing the direction
-		#Updates it
-		if saveDir.x != 0 || saveDir.y != 0:
-			currDir = saveDir
-			change_sprite()
-			saveDir = Vector2i(0,0)
+	#If there´s some saved dir, updates train´s dir
+	if distanceNext < 2:
 		currTile = nextTile
-		nextTile += currDir
-		
+		changeDir(saveDir)
 	#Ejecutar cambio de posición
 	global_position += currDir * speed * delta
+
+#If the player has been changing the direction
+#Updates it
+func changeDir(newDir: Vector2i) -> void:
+	global_position = tilemap.map_to_local(currTile)
+	if newDir.x != 0 || newDir.y != 0:
+			currDir = newDir
+			change_sprite()
+			saveDir = Vector2i(0,0)
+	nextTile = currTile + currDir
 
 func change_sprite() -> void:
 	#Change sprite depending on new direction
