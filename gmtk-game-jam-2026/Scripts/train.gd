18f762955af4d8a0bccea99307894062aea4ac39
Víctor Martínez Moreno <vicmar20@ucm.es>
2026-07-23 17:05:47 +0200

    added train sprites

--- gmtk-game-jam-2026/Scripts/train.gd
+++ gmtk-game-jam-2026/Scripts/train.gd
@@ -4,7 +4,6 @@ extends Node2D
 @export var iniTilePos: Vector2i
 @export var tilemap: TileMapLayer
 @export var sprite: Node
-@export var tracks : SpriteFrames
 
 var currTile: Vector2i
 # Saves the last input action which makes train changes direction
@@ -16,6 +15,7 @@ var nextTile: Vector2i
 var canChangeDir: bool
 #Distance to the currTile that makes train changes its direction inmediatly
 var fixDistance = 5
+var NextVia = true
 
 func _ready() -> void:
 	#Init current Tile and the next one
@@ -32,8 +32,8 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	var input_vector = Input.get_vector("Left", "Right", "Up", "Down")
-	
 	var distanceCurr = global_position.distance_to(tilemap.map_to_local(currTile))
+	
 	#Train´s direction changes
 	if input_vector != Vector2.ZERO:
 		#The train can´t go backwards or forward
@@ -47,6 +47,12 @@ func _process(delta: float) -> void:
 			if distanceCurr < fixDistance :
 				changeDir(Vector2i(input_vector.x, 0))
 			else: saveDir = Vector2i(input_vector.x, 0)
+	
+	if distanceCurr > fixDistance and NextVia:
+		var degress: int = 0
+		if currDir.y != 0: degress = 90
+		put_track(nextTile, true, degress)
+	
 	#When you arrive the next Tile
 	#Updates current and next Tile indexes
 	#Repositions the train on the new tile (just in case)
@@ -55,7 +61,9 @@ func _process(delta: float) -> void:
 	if distanceNext < 2:
 		currTile = nextTile
 		canChangeDir = true
+		NextVia = true
 		changeDir(saveDir)
+		
 	#Ejecutar cambio de posición
 	global_position += currDir * speed * delta
 
@@ -67,7 +75,7 @@ func changeDir(newDir: Vector2i) -> void:
 			currDir = newDir
 			var degrees:int = 0
 			if currDir.y != 0: degrees = 90
-			put_track(currTile+currDir, true, degrees)
+			put_track(currTile + currDir, true, degrees)
 			change_sprite()
 			saveDir = Vector2i(0,0)
 			#If you make any move you need to wait the next tile to do another
@@ -85,50 +93,26 @@ func change_sprite() -> void:
 		sprite.flip_h = currDir.x == -1
 
 func put_track(tile: Vector2i, straight: bool, degrees: int):
+	if not NextVia: return
+	
 	var tile_data = tilemap.get_cell_tile_data(tile)
 	if !tile_data:
 		#If didnt exist, creates it.
 		tilemap.set_cell(tile, 0, Vector2i(0, 0))
 		tile_data = tilemap.get_cell_tile_data(tile)
-		
-		#Select sprite
-		var track : Texture2D
-		if straight: track = tracks.get_frame_texture("straight", 0)
-		else: track = tracks.get_frame_texture("curve", 0)
-		
-		#Put the sprite
-		putTexture(tile, track, degrees)
-	else:
-		pass
 
-func putTexture(tile: Vector2i, tex: Texture2D, degrees: int)->void:
-		var atlas_source = tilemap.tile_set.get_source(0) as TileSetAtlasSource
-		var alternative_id = atlas_source.create_alternative_tile(Vector2i(0,0))
-		var alt_data = atlas_source.get_tile_data(Vector2i(0,0), alternative_id)
-		alt_data.texture_origin = Vector2i(0,0) 
-		
-		#Rotate n degrees
-		rotateTile(alt_data, degrees)
-		# Guardamos tu textura en el custom data si quieres conservarla como referencia lógica
-		alt_data.set_custom_data("textura", tex) 
-		# 5. Pintamos la celda usando el ID único de la textura que acabamos de crear en memoria
-		tilemap.set_cell(tile, 0, Vector2i(0, 0), alternative_id)
-
-func rotateTile(data: TileData, degrees: int) -> void:
+	#Select sprite on the atlas
+	var track : Vector2i
+	if straight: track = Vector2i(5, 0)
+	else: track = Vector2i(0, 0)
+	
+	#Rotate n degrees
+	var alternative_id: int = 0
 	match degrees:
-			90:
-				data.flip_h = true
-				data.flip_v = false
-				data.transpose = true
-			180:
-				data.flip_h = true
-				data.flip_v = true
-				data.transpose = false
-			270:
-				data.flip_h = false
-				data.flip_v = true
-				data.transpose = true
-			_: # default
-				data.flip_h = false
-				data.flip_v = false
-				data.transpose = false
+		90:  alternative_id = TileSetAtlasSource.TRANSFORM_FLIP_H | TileSetAtlasSource.TRANSFORM_TRANSPOSE
+		180: alternative_id = TileSetAtlasSource.TRANSFORM_FLIP_H | TileSetAtlasSource.TRANSFORM_FLIP_V
+		270: alternative_id = TileSetAtlasSource.TRANSFORM_FLIP_V | TileSetAtlasSource.TRANSFORM_TRANSPOSE
+		_: alternative_id = 0 #default
+		
+	tilemap.set_cell(tile, 0, track, alternative_id)
+	NextVia = false
