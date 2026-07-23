549e334d7a5e4bb956f97e000b84fed05d5ea207
OscarDaF <carlitosrvmvk17@gmail.com>
2026-07-23 17:21:31 +0200

    Tile single detection

--- gmtk-game-jam-2026/Scenes/Levels/trainCollisions.gd
+++ gmtk-game-jam-2026/Scenes/Levels/trainCollisions.gd
@@ -2,6 +2,8 @@ extends Node2D
 @export var obstaclesMap: TileMapLayer
 @export var train: Node2D
 
+var storedTile : TileData
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,4 +11,22 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	checkIfInsideTile()
 	pass
+
+
+func checkIfInsideTile() ->void:
+	var currentTile = obstaclesMap.local_to_map(obstaclesMap.to_local(train.global_position))
+	#print(currentTile)
+	var tileData=obstaclesMap.get_cell_tile_data(currentTile);
+	
+	if tileData && tileData != storedTile:
+		print("inside Tile")
+		tileEvent()
+	storedTile = tileData
+	
+	
+	
+
+func tileEvent() -> void:
+	print ("Executing tile event")
