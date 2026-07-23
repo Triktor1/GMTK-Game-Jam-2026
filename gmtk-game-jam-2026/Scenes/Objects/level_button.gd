57512ffa22524860ef1996e2b598931e4c2a432d
Adrián de Miguel Cerezo <adriadem@ucm.es>
2026-07-23 12:26:58 +0200

    level selector

---
+++ gmtk-game-jam-2026/Scenes/Objects/level_button.gd
@@ -0,0 +1,20 @@
+extends Button
+
+@export_enum("1") var scene_to_load:int
+
+const LEVEL_1:String="res://Scenes/Levels/level_1.tscn"
+
+
+func _ready() -> void:
+	pressed.connect(_load_scene)
+
+
+
+
+func _load_scene():
+	var level:String
+	match scene_to_load:
+		"1":
+			level=LEVEL_1
+	if level:
+		get_tree().change_scene_to_file(level)
