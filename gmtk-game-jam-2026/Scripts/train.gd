b4aacb9dc69b6c5d8a1e2bf9387e38bfa404eed8
Víctor Martínez Moreno <vicmar20@ucm.es>
2026-07-23 21:36:17 +0200

    train stuff

--- gmtk-game-jam-2026/Scripts/train.gd
+++ gmtk-game-jam-2026/Scripts/train.gd
@@ -38,12 +38,7 @@ func _process(delta: float) -> void:
 			saveDir = Vector2i(0, input_vector.y)
 		elif input_vector.y == 0 and abs(input_vector.x) != abs(currDir.x):
 			saveDir = Vector2i(input_vector.x, 0)
-	
-	#We can change direction if we arrive the new tile or
-	#we are some fixed distance away
-	if distanceCurr < fixDistance:
-		changeDir(saveDir)
-	
+
 	#When we reach the new Tile
 	# Updates current and next Tiles
 	# Allow the player to change direction
@@ -51,16 +46,22 @@ func _process(delta: float) -> void:
 	if distanceNext < fixDistance:
 		currTile = nextTile
 		canChangeDir = true
-		NextVia = true
 		nextTile = currTile + currDir
+		if not onTrack():
+			NextVia = true
+			changeDir(saveDir)
+	#We can change direction if we arrive the new tile or
+	#if we are some fixed distance away
+	if distanceCurr < fixDistance:
 		changeDir(saveDir)
-	
+
 	#Moves the train forward
 	global_position += Vector2(currDir) * speed * delta
 	
 	#Once we have moved, We can set up the next track.
 	#Thats if the distance to the current Tile is enought away
 	#so the player cant change the direction before the next tile
+	distanceCurr = global_position.distance_to(tilemap.map_to_local(currTile))
 	if distanceCurr > fixDistance and NextVia:
 		var degress: int = 0
 		if currDir.y != 0: degress = 90
@@ -71,24 +72,25 @@ func _process(delta: float) -> void:
 # Changes the train direction to the especify direction
 # excepts null direction, cause the train is always moving
 # or if the player has already changed the direction on the same tile
-func changeDir(newDir: Vector2i) -> void:
+func changeDir(newDir: Vector2i, onTrack:bool = false) -> void:
 	if not canChangeDir or (newDir.x == 0 and newDir.y == 0): return
 	#To change direction, we update last directon and current direction
 	#Relocate the train
 	#Update the last track to make it curve
 	#We know player cant change Direction until the next tile, so we can put the next track
 	#Also changes the train sprite and resets saved direction
-	global_position = tilemap.map_to_local(currTile)
+	if newDir != currDir: global_position = tilemap.map_to_local(currTile)
 	
 	lastDir = currDir
 	currDir = newDir
 	
-	# When we change direction, the last track changes to be a curve track
-	try_change_track(currTile)
-	# And we can place the next track cause the player cant change the direction on this tile
-	var degrees: int = 0
-	if currDir.y != 0: degrees = 90
-	put_track(currTile, true, degrees)
+	if not onTrack:
+		# When we change direction, the last track changes to be a curve track
+		try_change_track(currTile)
+		# And we can place the next track cause the player cant change the direction on this tile
+		var degrees: int = 0
+		if currDir.y != 0: degrees = 90
+		put_track(currTile, true, degrees)
 	
 	change_sprite()
 	#Resets saved direction
@@ -156,3 +158,26 @@ func put_track(tile: Vector2i, straight: bool, degrees: int, replace:bool = fals
 		# Place the new texture on the tileMap
 		tilemap.set_cell(tile, 0, track, alternative_id)
 		if not replace: NextVia = false
+
+func onTrack() -> bool :
+	var tile_data = tilemap.get_cell_tile_data(currTile)
+	if not tile_data or not NextVia: return false
+	
+	var textureAtlas = tilemap.get_cell_atlas_coords(currTile)
+	var degrees: int = tilemap.get_cell_alternative_tile(currTile)
+	match degrees:
+		TileSetAtlasSource.TRANSFORM_FLIP_H | TileSetAtlasSource.TRANSFORM_TRANSPOSE: degrees = 90
+		TileSetAtlasSource.TRANSFORM_FLIP_H | TileSetAtlasSource.TRANSFORM_FLIP_V: degrees = 180
+		TileSetAtlasSource.TRANSFORM_FLIP_V | TileSetAtlasSource.TRANSFORM_TRANSPOSE: degrees = 270
+		_: degrees = 0
+
+	if textureAtlas == Vector2i(1,0):
+		pass
+	elif textureAtlas == Vector2i(5,0):
+		if (currDir.x != 0 and degrees == 90) or (currDir.y != 0 and degrees == 0): explode()
+		elif (currDir.x != 0 and degrees == 0) or (currDir.y != 0 and degrees == 90): changeDir(currDir, true)
+	return true
+
+func explode() -> void:
+	currDir = Vector2i(0,0)
+	print("EXPLODE")
