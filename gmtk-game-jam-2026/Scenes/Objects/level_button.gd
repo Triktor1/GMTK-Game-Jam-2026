b2713063320e11b6d1bf68b3a56cc6258b80cbb4
Adrián de Miguel Cerezo <adriadem@ucm.es>
2026-07-23 12:39:50 +0200

    oops

--- gmtk-game-jam-2026/Scenes/Objects/level_button.gd
+++ gmtk-game-jam-2026/Scenes/Objects/level_button.gd
@@ -1,6 +1,6 @@
 extends Button
 
-@export_enum("1") var scene_to_load:int
+@export_enum("1") var scene_to_load:String
 
 const LEVEL_1:String="res://Scenes/Levels/level_1.tscn"
 
@@ -9,12 +9,10 @@ func _ready() -> void:
 	pressed.connect(_load_scene)
 
 
-
-
 func _load_scene():
-	var level:String
+	var level:String=""
 	match scene_to_load:
 		"1":
 			level=LEVEL_1
-	if level:
+	if level!="":
 		get_tree().change_scene_to_file(level)
