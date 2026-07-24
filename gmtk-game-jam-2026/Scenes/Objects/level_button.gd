a7b86a47d706a6fb36f14e263b4201c96e1e4f22
Adrián de Miguel Cerezo <adriadem@ucm.es>
2026-07-24 18:33:33 +0200

    añadido que los botones de niveles 1 a 5 te lleven a los niveles 1 a 5

--- gmtk-game-jam-2026/Scenes/Objects/level_button.gd
+++ gmtk-game-jam-2026/Scenes/Objects/level_button.gd
@@ -1,8 +1,12 @@
 extends Button
 
-@export_enum("1") var scene_to_load:String
+@export_enum("1","2","3","4","5") var scene_to_load:String
 
 const LEVEL_1:String="res://Scenes/Levels/level_1.tscn"
+const LEVEL_2:String="res://Scenes/Levels/level_2.tscn"
+const LEVEL_3:String="res://Scenes/Levels/level_3.tscn"
+const LEVEL_4:String="res://Scenes/Levels/level_4.tscn"
+const LEVEL_5:String="res://Scenes/Levels/level_5.tscn"
 
 
 func _ready() -> void:
@@ -14,5 +18,13 @@ func _load_scene():
 	match scene_to_load:
 		"1":
 			level=LEVEL_1
+		"2":
+			level=LEVEL_2
+		"3":
+			level=LEVEL_3
+		"4":
+			level=LEVEL_4
+		"5":
+			level=LEVEL_5
 	if level!="":
 		get_tree().change_scene_to_file(level)
