1c8e8fd1dc9c70c0cb2d6f19af04e93b9f86791d
Triktor <yosoyvictor06@gmail.com>
2026-07-24 13:02:48 +0200

    MOOOORE THINGS for container tween
    fixed a bug, added more animation types, removed exit signal (it was really ugly)

--- gmtk-game-jam-2026/Scripts/container_tween.gd
+++ gmtk-game-jam-2026/Scripts/container_tween.gd
@@ -12,12 +12,18 @@ enum ANIM_TYPE{
 	SLIDE_IN_LEFT,
 	##Elements will slide from the right
 	SLIDE_IN_RIGHT,
+	SLIDE_IN_TOP,
+	SLIDE_IN_BOTTOM,
 	##Elements will scale from 0 to 1
 	SCALE,
 	##Elements will alternate between sliding from the left and right, starting in the left
 	SHUFFLE_FROM_LEFT,
 	##Elements will alternate between sliding from the left and right, starting in the right
-	SHUFFLE_FROM_RIGHT
+	SHUFFLE_FROM_RIGHT,
+	##Elements will alternate between sliding from the top and bottom, starting in the top
+	SHUFFLE_FROM_TOP,
+	##Elements will alternate between sliding from the top and bottom, starting in the bottom
+	SHUFFLE_FROM_BOTTOM
 }
 
 enum ORDER_TYPE{
@@ -45,8 +51,6 @@ enum SCALE_FROM{
 @export var anim_when: ANIM_WHEN = ANIM_WHEN.READY
 ## Name of the signal that will be emitted to start aniamtion (only used if anim_when = SIGNAL)
 @export var enter_signal_name: StringName = "enter_signal"
-## Name of the signal that will be emitted to reset object modulation (this one is always used)
-@export var exit_signal_name: StringName = "exit_signal"
 ## Type of animation
 @export var anim_type: ANIM_TYPE = ANIM_TYPE.SCALE
 ##The order in which the elements will be animated
@@ -61,6 +65,7 @@ enum SCALE_FROM{
 @export var delay_between_elems: float = 0.05
 
 var tween: Tween
+var original_positions: Array[Vector2]
 
 func _ready() -> void:
 	# If target isn't assigned, defaults to parent node
@@ -69,6 +74,9 @@ func _ready() -> void:
 	
 	# Assume nodes can't have more than one container_tween
 	target.set_meta("container_tween", self)
+	
+	for c: Control in get_children():
+		original_positions.push_back(c.position)
 	
 	match anim_type:
 		ANIM_TYPE.SCALE:
@@ -88,8 +96,6 @@ func _ready() -> void:
 		appear.call_deferred()
 	elif anim_when == ANIM_WHEN.SIGNAL:
 		EventBus.connect_signal(enter_signal_name, appear)
-		
-	EventBus.connect_signal(exit_signal_name, disappear)
 
 func set_pivot(control: Control, pivot: SCALE_FROM) -> void:
 	match pivot:
@@ -113,6 +119,7 @@ func set_pivot(control: Control, pivot: SCALE_FROM) -> void:
 			control.pivot_offset = Vector2(control.size.x / 2.0, control.size.y / 2.0)
 
 func appear() -> void:
+	disappear()
 	var children: Array = target.get_children()
 	
 	#Call set_pivot again in case size changed
@@ -122,6 +129,10 @@ func appear() -> void:
 	#If tween is already doing something we KILL IT RUTHLESSLY (and remake it)
 	if tween && tween.is_running():
 		tween.kill()
+		var i: int = 0
+		for c: Control in children:
+			c.position = original_positions[i]
+			i+=1
 	tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
 	tween.set_parallel(true)
 	
@@ -131,26 +142,39 @@ func appear() -> void:
 	
 	if order_type == ORDER_TYPE.START_BOTTOM:
 		children.reverse()
+		original_positions.reverse()
 		
 	var cont: int = -1
-	if anim_type == ANIM_TYPE.SHUFFLE_FROM_RIGHT:
+	if anim_type == ANIM_TYPE.SHUFFLE_FROM_RIGHT || anim_type == ANIM_TYPE.SHUFFLE_FROM_BOTTOM:
 		cont *= -1
 		
 	var idx: int = 0
 	for c: Control in children:
-		if anim_type == ANIM_TYPE.SCALE:
-			tween.tween_property(c, "scale", Vector2.ONE, duration).from(Vector2.ZERO).set_delay(delay_between_elems*idx)
-			tween.tween_property(c, "modulate:a", 1.0, 0.01).set_delay(delay_between_elems*idx)
-		elif anim_type == ANIM_TYPE.SLIDE_IN_LEFT:
-			tween.tween_property(c, "position:x", c.position.x, duration).from(c.position.x-c.size.x).set_delay(delay_between_elems*idx)
-			tween.tween_property(c, "modulate:a", 1.0, 0.05).set_delay(delay_between_elems*idx)
-		elif anim_type == ANIM_TYPE.SLIDE_IN_RIGHT:
-			tween.tween_property(c, "position:x", c.position.x, duration).from(c.position.x+c.size.x).set_delay(delay_between_elems*idx)
-			tween.tween_property(c, "modulate:a", 1.0, 0.05).set_delay(delay_between_elems*idx)
-		elif anim_type == ANIM_TYPE.SHUFFLE_FROM_LEFT || anim_type == ANIM_TYPE.SHUFFLE_FROM_RIGHT:
-			tween.tween_property(c, "position:x", c.position.x, duration).from(c.position.x+(c.size.x*cont)).set_delay(delay_between_elems*idx)
-			tween.tween_property(c, "modulate:a", 1.0, 0.05).set_delay(delay_between_elems*idx)
-			cont *= -1
+		match anim_type:
+			ANIM_TYPE.SCALE:
+				tween.tween_property(c, "scale", Vector2.ONE, duration).from(Vector2.ZERO).set_delay(delay_between_elems*idx)
+				tween.tween_property(c, "modulate:a", 1.0, 0.01).set_delay(delay_between_elems*idx)
+			ANIM_TYPE.SLIDE_IN_LEFT:
+				tween.tween_property(c, "position:x", c.position.x, duration).from(c.position.x-c.size.x).set_delay(delay_between_elems*idx)
+				tween.tween_property(c, "modulate:a", 1.0, 0.05).set_delay(delay_between_elems*idx)
+			ANIM_TYPE.SLIDE_IN_RIGHT:
+				tween.tween_property(c, "position:x", c.position.x, duration).from(c.position.x+c.size.x).set_delay(delay_between_elems*idx)
+				tween.tween_property(c, "modulate:a", 1.0, 0.05).set_delay(delay_between_elems*idx)
+			ANIM_TYPE.SLIDE_IN_TOP:
+				tween.tween_property(c, "position:y", c.position.y, duration).from(c.position.y-c.size.y).set_delay(delay_between_elems*idx)
+				tween.tween_property(c, "modulate:a", 1.0, 0.05).set_delay(delay_between_elems*idx)
+			ANIM_TYPE.SLIDE_IN_BOTTOM:
+				tween.tween_property(c, "position:y", c.position.y, duration).from(c.position.y+c.size.y).set_delay(delay_between_elems*idx)
+				tween.tween_property(c, "modulate:a", 1.0, 0.05).set_delay(delay_between_elems*idx)
+			ANIM_TYPE.SHUFFLE_FROM_LEFT, ANIM_TYPE.SHUFFLE_FROM_RIGHT:
+				tween.tween_property(c, "position:x", c.position.x, duration).from(c.position.x+(c.size.x*cont)).set_delay(delay_between_elems*idx)
+				tween.tween_property(c, "modulate:a", 1.0, 0.05).set_delay(delay_between_elems*idx)
+				cont *= -1
+			ANIM_TYPE.SHUFFLE_FROM_TOP, ANIM_TYPE.SHUFFLE_FROM_BOTTOM:
+				tween.tween_property(c, "position:y", c.position.x, duration).from(c.position.y+(c.size.y*cont)).set_delay(delay_between_elems*idx)
+				tween.tween_property(c, "modulate:a", 1.0, 0.05).set_delay(delay_between_elems*idx)
+				cont *= -1
+			
 		
 		idx += 1
 
