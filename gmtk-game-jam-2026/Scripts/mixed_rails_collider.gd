026dbf75a912cde8f25bcce8e216a41ada813658
Ribir24 <Victoralvarez_24@icloud.com>
2026-07-25 11:03:17 +0200

    Mixed rails con textura bnueva

--- gmtk-game-jam-2026/Scripts/mixed_rails_collider.gd
+++ gmtk-game-jam-2026/Scripts/mixed_rails_collider.gd
@@ -17,7 +17,7 @@ func onTrack() -> bool :
 		TileSetAtlasSource.TRANSFORM_FLIP_V | TileSetAtlasSource.TRANSFORM_TRANSPOSE: degrees = 270
 		_: degrees = 0
 	
-	if textureAtlas == Vector2i(1,0): #curvado, de izq a arr
+	if textureAtlas == Vector2i(11,0): #curvado, de izq a arr
 		if degrees == 0:
 			if train.currDir == Vector2i(0, 1): train.changeDir(Vector2i(-1,0), true)
 			elif train.currDir == Vector2i(1, 0): train.changeDir(Vector2i(0,-1), true)
@@ -34,15 +34,15 @@ func onTrack() -> bool :
 			if train.currDir == Vector2i(0, -1): train.changeDir(Vector2i(-1,0), true)
 			elif train.currDir == Vector2i(1, 0): train.changeDir(Vector2i(0,1), true)
 			else: train.explode()
-	elif textureAtlas == Vector2i(2,0) or textureAtlas == Vector2i(3,0): # asumimos que es el horizontal recto O horizontal recto con boton
+	elif textureAtlas == Vector2i(6,0) or textureAtlas == Vector2i(8,0): # asumimos que es el horizontal recto O horizontal recto con boton
 		if (train.currDir.x != 0 and degrees == 90) or (train.currDir.y != 0 and degrees == 0): train.explode()
 		elif (train.currDir.x != 0 and degrees == 0) or (train.currDir.y != 0 and degrees == 90): train.changeDir(train.currDir, true)
-		if textureAtlas == Vector2i(3,0) and not train.isCargo: #si tiene boton y no es cargo, gira todas.
+		if textureAtlas == Vector2i(8,0) and not train.isCargo: #si tiene boton y no es cargo, gira todas.
 				for cell in train.tilemapTracks.get_used_cells():
 					var atlas: Vector2i = train.tilemapTracks.get_cell_atlas_coords(cell)
 					var alt : int = train.tilemapTracks.get_cell_alternative_tile(cell)
-					if atlas == Vector2i(1,0):
-						train.tilemapTracks.set_cell(cell, 0, Vector2i(2,0), alt)
-					elif atlas == Vector2i(2,0):
-						train.tilemapTracks.set_cell(cell, 0, Vector2i(1,0), alt)
+					if atlas == Vector2i(11,0):
+						train.tilemapTracks.set_cell(cell, 0, Vector2i(6,0), alt)
+					elif atlas == Vector2i(6,0):
+						train.tilemapTracks.set_cell(cell, 0, Vector2i(11,0), alt)
 	return true
