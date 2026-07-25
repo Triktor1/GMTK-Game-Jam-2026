3d2a43af817de661f96183ba44012e63a164bb40
Ribir24 <Victoralvarez_24@icloud.com>
2026-07-25 13:38:47 +0200

    vias con palanca

--- gmtk-game-jam-2026/Scenes/Levels/trainCollisions.gd
+++ gmtk-game-jam-2026/Scenes/Levels/trainCollisions.gd
@@ -8,6 +8,7 @@ var storedTile : TileData
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	EventBus.connect_signal("getCargo", eraseAdyacentObstacle)
+	EventBus.connect_signal("pullLever", pullAdyacentLever)
 	pass # Replace with function body.
 
 
@@ -35,6 +36,21 @@ func eraseAdyacentObstacle(tile: Vector2i, id: int) -> void:
 	if obstaclesMap.get_cell_tile_data(tile+Vector2i(-1,0)): if obstaclesMap.get_cell_source_id(tile+Vector2i(-1,0)) == id: obstaclesMap.erase_cell(tile+Vector2i(-1,0))
 	if obstaclesMap.get_cell_tile_data(tile+Vector2i(0,1)): if obstaclesMap.get_cell_source_id(tile+Vector2i(0,1)) == id: obstaclesMap.erase_cell(tile+Vector2i(0,1))
 	if obstaclesMap.get_cell_tile_data(tile+Vector2i(0,-1)): if obstaclesMap.get_cell_source_id(tile+Vector2i(0,-1)) == id: obstaclesMap.erase_cell(tile+Vector2i(0,-1))
+
+func pullAdyacentLever(tile: Vector2i, id: int) -> void:
+	var directions = [Vector2i(1, 0), Vector2i(-1, 0), Vector2i(0, 1), Vector2i(0, -1)]
+	for dir in directions:
+		var pos = tile + dir
+		if obstaclesMap.get_cell_tile_data(pos) and obstaclesMap.get_cell_source_id(pos) == id:
+			var atlas = obstaclesMap.get_cell_atlas_coords(pos)
+			var alternative = obstaclesMap.get_cell_alternative_tile(pos)
+			if id == 0:
+				obstaclesMap.set_cell(pos, 1, atlas, alternative)
+			elif id == 1:
+				obstaclesMap.set_cell(pos, 0, atlas, alternative)
+			else: #Invierte horizontalmente el sprite
+				obstaclesMap.set_cell(pos, id, atlas, alternative ^ TileSetAtlasSource.TRANSFORM_FLIP_H)
+
 func tileEvent() -> void:
 	EventBus.emit("explode", [])
 	print ("Executing tile event")
--- gmtk-game-jam-2026/Scripts/train.gd
+++ gmtk-game-jam-2026/Scripts/train.gd
@@ -5,6 +5,7 @@ extends Node2D
 @export var iniPosOffset: Vector2
 @export var tilemapTracks: TileMapLayer
 @export var tilemapCargos: TileMapLayer
+@export var tilemapLevers: TileMapLayer
 @export var cargo: PackedScene
 @export var sprite: Node
 @export var isCargo: bool
@@ -69,7 +70,9 @@ func _process(delta: float) -> void:
 		if not onTrack():
 			NextVia = true
 			changeDir(saveDir)
-		if not exploded: onCargo()
+		if not exploded:
+			onLever()
+			onCargo()
 	
 	#We can change direction if we arrive the new tile or
 	#if we are some fixed distance away
@@ -162,6 +165,7 @@ func put_track(tile: Vector2i, straight: bool, degrees: int, replace:bool = fals
 	
 	var tile_data = tilemapTracks.get_cell_tile_data(tile)
 	if not tile_data: tile_data = tilemapCargos.get_cell_tile_data(tile)
+	if not tile_data: tile_data = tilemapLevers.get_cell_tile_data(tile)
 	
 	if !tile_data or replace:
 		if wthoutTracks:
@@ -198,8 +202,13 @@ func onTrack() -> bool :
 	if not tile_data:
 		tile_map = tilemapCargos
 		tile_data = tile_map.get_cell_tile_data(currTile)
+		
+		if not tile_data:
+			tile_map = tilemapLevers
+			tile_data = tile_map.get_cell_tile_data(currTile)
+			
 		if not tile_data : return false
-	
+		
 	var i := 0
 	while i < get_child_count():
 		var child = get_child(i)
@@ -253,6 +262,17 @@ func onCargo() -> bool :
 	createCargo()
 	return true
 
+func onLever() -> bool :
+	var tile_data = tilemapLevers.get_cell_tile_data(currTile)
+	if not tile_data: return false
+	
+	var textureAtlas = tilemapLevers.get_cell_atlas_coords(currTile)
+	
+	if textureAtlas != Vector2i(5,0):
+		return false
+	
+	EventBus.emit("pullLever", [currTile, 5])
+	return true
 
 func createCargo() -> void:
 	if myCargo:
