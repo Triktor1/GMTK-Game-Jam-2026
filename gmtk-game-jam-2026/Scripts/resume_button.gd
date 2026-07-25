e74b9a2b33d7681ccfe04d7781fbe5f198f59cfd
Víctor Martínez Moreno <vicmar20@ucm.es>
2026-07-26 00:53:40 +0200

    e

--- gmtk-game-jam-2026/Scripts/resume_button.gd
+++ gmtk-game-jam-2026/Scripts/resume_button.gd
@@ -5,5 +5,6 @@ func _ready():
 	pressed.connect(_on_pressed)
 
 func _on_pressed():
+	get_parent().get_parent().visible = false
 	get_tree().call_group("level", "_on_resume_requested")
 	EventBus.emit("exit_pause")
