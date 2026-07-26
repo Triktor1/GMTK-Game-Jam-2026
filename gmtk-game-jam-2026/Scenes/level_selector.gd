f93c4a9e64ce3762dfcaaee80dde007f85051a30
ossilv01 <ossilv01@ucm.es>
2026-07-26 12:02:39 +0200

    2000 años tarde

--- gmtk-game-jam-2026/Scenes/level_selector.gd
+++ gmtk-game-jam-2026/Scenes/level_selector.gd
@@ -6,5 +6,13 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var levels:Array=grid.get_children()
+	
 	for i in range(0,LevelManager.max_level):
 		levels[i].unlock()
+		
+	for button: Button in grid.get_children():
+		button.focus_neighbor_bottom = main_menu.get_path()
+		
+		button.focus_entered.connect(func():
+			main_menu.focus_neighbor_top = button.get_path())
+	levels[0].grab_focus()
